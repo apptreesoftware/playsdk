d39a4b6c854236c37a882d6f37042adf1d66c767
Matthew Smith <matthew.smith@apptreesoftware.com>
2016-05-10 10:31:13 -0700

    added stubs for description

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,12 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import sdk.AppTree;
 
 /**
  * Created by matthew on 5/10/16.
  */
 public class Application extends Controller {
     public Result describe() {
-        AppTree.
+        return ok();
     }
 }
