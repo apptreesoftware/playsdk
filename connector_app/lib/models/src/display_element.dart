d65ed332c0f93cf188e00d8693524cb46d98831c
Matthew Smith <matthew.smith@apptreesoftware.com>
2016-05-12 12:37:20 -0700

    Removing play infrastructure keeping only the SDK classes.

--- connector_app/lib/models/src/display_element.dart
+++
@@ -1,8 +0,0 @@
-part of connector_app.models;
-
-abstract class DisplayElement {
-  DataType get dataType;
-  DisplayType get displayType;
-  String get title;
-  bool get hidden;
-}
--- connector_app/web/index.dart
+++
@@ -1,8 +0,0 @@
-import 'package:polymer/polymer.dart';
-import 'package:connector_app/components/connector_app.dart';
-
-/// silence analyzer warnings:
-/// [ConnectorAppElement]
-main() async {
-  await initPolymer();
-}
