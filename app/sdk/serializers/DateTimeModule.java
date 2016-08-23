c8de3d839ef2e32798b47685f694480cdfceb439
Alexis Andreason <alexis.andreason@apptreesoftware.com>
2016-08-22 19:34:59 -0700

    renamed DateTimeModule's date time serializer to deserializer and added a serializer

--- app/sdk/serializers/DateTimeModule.java
+++ app/sdk/serializers/DateTimeModule.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.ContextualSerializer;
 import org.joda.time.DateTime;
 import sdk.data.DataSet;
+import sdk.utils.Constants;
 import sdk.utils.DateUtil;
 
 import java.io.IOException;
@@ -16,7 +18,7 @@ import java.io.IOException;
  * Created by mellissatort on 5/25/16.
  */
 public class DateTimeModule extends SimpleModule {
-    private class DateTimeSerializer extends JsonDeserializer<DateTime> implements ContextualDeserializer {
+    private class DateTimeDeserializer extends JsonDeserializer<DateTime> implements ContextualDeserializer {
 
         @Override
         public DateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
@@ -30,11 +32,25 @@ public class DateTimeModule extends SimpleModule {
 
         @Override
         public JsonDeserializer<?> createContextual(DeserializationContext ctxt, BeanProperty property) throws JsonMappingException {
+            return new DateTimeDeserializer();
+        }
+    }
+
+    private class DateTimeSerializer extends JsonSerializer<DateTime> implements ContextualSerializer {
+
+        @Override
+        public void serialize(DateTime value, JsonGenerator gen, SerializerProvider arg2) throws IOException, JsonProcessingException {
+            gen.writeString(Constants.AppTreeDateTimeFormat.print(value));
+        }
+
+        @Override
+        public JsonSerializer<DateTime> createContextual(SerializerProvider prov, BeanProperty property) throws JsonMappingException {
             return new DateTimeSerializer();
         }
     }
 
     public DateTimeModule() {
-        addDeserializer(DateTime.class, new DateTimeSerializer());
+        addDeserializer(DateTime.class, new DateTimeDeserializer());
+        addSerializer(DateTime.class, new DateTimeSerializer());
     }
 }
