4d2f32431161cd3073c306b5280692e9ce6d5665
Alexis Andreason <alexis.andreason@apptreesoftware.com>
2016-09-15 10:06:06 -0700

    no message

--- app/sdk/user/UserInfoKey.java
+++ app/sdk/user/UserInfoKey.java
@@ -10,7 +10,8 @@ public class UserInfoKey {
     public String id;
     public AttributeType attributeType;
 
-    public UserInfoKey(String name, AttributeType attributeType) {
+    public UserInfoKey(String id, String name, AttributeType attributeType) {
+        this.id = id;
         this.name = name;
         this.attributeType = attributeType;
     }
