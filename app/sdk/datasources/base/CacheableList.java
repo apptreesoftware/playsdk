99652402d733a9ff8537935f21676707bf0758ec
Matthew Smith <matthew.smith@apptreesoftware.com>
2016-11-03 14:54:34 -0700

    All cacheable lists have a default search list implementation that returns all results

--- app/sdk/datasources/base/CacheableList.java
+++ app/sdk/datasources/base/CacheableList.java
@@ -5,10 +5,17 @@ import sdk.datasources.ListDataSource;
 import sdk.utils.AuthenticationInfo;
 import sdk.utils.Parameters;
 
+import java.util.Map;
+
 /**
  * Created by alexis on 5/10/16.
  */
-public interface CacheableList extends ListDataSource {
+public interface CacheableList extends ListDataSource, SearchableList {
     List getList(AuthenticationInfo authenticationInfo, Parameters params);
     boolean isListContentGlobal();
+
+    @Override
+    default List queryList(String queryText, boolean barcodeSearch, Map<String, Object> searchParameters, AuthenticationInfo authenticationInfo, Parameters params) {
+        return getList(authenticationInfo, params);
+    }
 }
