4b11c1d5bec3f841e481b35a9540453ad60222ad
Alexis Andreason <alexis.andreason@apptreesoftware.com>
2017-05-26 15:02:03 -0700

    updated send batch method to set appropriate callback headers

--- app/sdk/utils/BatchManager.java
+++ app/sdk/utils/BatchManager.java
@@ -2,6 +2,7 @@ package sdk.utils;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.Logger;
+import play.libs.Json;
 import play.libs.ws.WSClient;
 import play.libs.ws.WSRequest;
 import sdk.data.DataSet;
@@ -23,7 +24,16 @@ public class BatchManager {
     public void sendBatch(DataSet dataSet, boolean moreRecordsAvailable) {
         dataSet.setMoreRecordsAvailable(moreRecordsAvailable);
         WSRequest request = client.url(callbackURL);
-        ObjectNode json = dataSet.toJSON();
+        ObjectNode json;
+        if (dataSet.isSuccess()) {
+            request.setHeader(Constants.CORE_CALLBACK_TYPE, Constants.CORE_CALLBACK_TYPE_SUCCESS);
+        } else {
+            request.setHeader(Constants.CORE_CALLBACK_TYPE, Constants.CORE_CALLBACK_TYPE_ERROR);
+            json = Json.newObject();
+            json.put(Constants.CORE_CALLBACK_MESSAGE, dataSet.getMessage() != null ? dataSet.getMessage() : "");
+            request.setBody(json);
+        }
+        json = dataSet.toJSON();
         Logger.debug(String.format("Async response: %s\n%s", callbackURL, json.toString()));
         Logger.debug(json.toString());
         request.post(json)
