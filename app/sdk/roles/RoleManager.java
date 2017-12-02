fcd4e7680949ccc9da7c9eef424672b1d2a9934a
Sam Orozco <sam.orozco@apptreesoftware.com>
2017-12-02 12:35:42 -0800

    Adding roles manager for getting roles fromg an appID

---
+++ app/sdk/roles/RoleManager.java
@@ -0,0 +1,81 @@
+package sdk.roles;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
+import play.libs.ws.WSResponse;
+import sdk.roles.models.AppRole;
+import sdk.utils.Constants;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+import static sdk.utils.ValidationUtils.NullOrEmpty;
+
+public class RoleManager {
+    private String programmaticUserToken;
+    private WSClient wsClient;
+    private static final String getRolesEndPoint = "http://localhost:9001/public/1/roles";
+    private static ObjectMapper objectMapper;
+
+    private RoleManager() {
+    }
+
+    private RoleManager(String programmaticUserToken, WSClient client) {
+        this.programmaticUserToken = programmaticUserToken;
+        this.wsClient = client;
+    }
+
+    public static RoleManager getInstance(String programmaticUserToken, WSClient client) {
+        return new RoleManager(programmaticUserToken, client);
+    }
+
+    public List<AppRole> getRolesForAppID(String appID) throws ExecutionException,
+                                                               InterruptedException {
+        WSRequest request = getAppRolesRequest(appID);
+        CompletionStage<WSResponse> response = request.get();
+        AppRole[] roles = response.thenApply(WSResponse::asJson)
+                                  .thenApply(this::jsonNodeToAppRoles)
+                                  .toCompletableFuture()
+                                  .get();
+        return Arrays.asList(roles);
+    }
+
+    public WSRequest getAppRolesRequest(String appID) {
+        if (wsClient == null) {
+            throw new RuntimeException("WS Client was not initialized correctly");
+        }
+        WSRequest request = wsClient.url(getRolesEndPoint);
+        if (NullOrEmpty(programmaticUserToken)) {
+            throw new RuntimeException("Programmatic user token was not initialized correctly");
+        }
+        request.setHeader(Constants.APP_ID_HEADER, appID);
+        request.setHeader(Constants.CONTENT_TYPE, Constants.APPLICATION_JSON);
+        request.setHeader(Constants.X_APPTREE_TOKEN_ID, this.programmaticUserToken);
+        return request;
+    }
+
+
+    private ObjectMapper getObjectMapper() {
+        if (objectMapper == null) {
+            objectMapper = new ObjectMapper();
+        }
+        return objectMapper;
+    }
+
+    private AppRole[] jsonNodeToAppRoles(JsonNode node) {
+        try {
+            return getObjectMapper().treeToValue(node, AppRole[].class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("There was an error parsing the roles response");
+        }
+    }
+
+
+}
---
+++ app/sdk/roles/models/AppRole.java
@@ -0,0 +1,31 @@
+package sdk.roles.models;
+
+public class AppRole {
+    private int roleId;
+    private String roleName;
+    private String appID;
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getAppID() {
+        return appID;
+    }
+
+    public void setAppID(String appID) {
+        this.appID = appID;
+    }
+}
