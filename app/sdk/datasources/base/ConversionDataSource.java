828be87c45731aec5ab56426f402cf602f1aa31a
Sam Orozco <sam.orozco@apptreesoftware.com>
2018-01-18 10:42:12 -0800

    updating isSearchForm value for conversion data source

--- app/sdk/datasources/base/ConversionDataSource.java
+++ app/sdk/datasources/base/ConversionDataSource.java
@@ -15,13 +15,13 @@ import java.util.ArrayList;
 public abstract class ConversionDataSource<S, D> implements ConversionDataSourceBase {
 
     public D convertRecord(DataSetItem dataSetItem, S object) {
-        ObjectConverter.copyFromRecord(dataSetItem, object, true);
+        ObjectConverter.copyFromRecord(dataSetItem, object, false);
         return convert(object);
     }
 
     public D convertRecord(DataSetItem previousDataSetItem, DataSetItem dataSetItem, S previousObject, S object) {
-        ObjectConverter.copyFromRecord(previousDataSetItem, previousObject, true);
-        ObjectConverter.copyFromRecord(dataSetItem, object, true);
+        ObjectConverter.copyFromRecord(previousDataSetItem, previousObject, false);
+        ObjectConverter.copyFromRecord(dataSetItem, object, false);
         return convert(previousObject, object);
     }
 
